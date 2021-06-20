0be9f6cd5292f7cfad9d809741d65c864210ae47
Mikolaj <m.zelek@op.pl>
2021-06-20 18:55:55 +0200

    Setup and versioning

---
+++ Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostChan.Contracts
+{
+    public static class ApiRoutes
+    {
+        public const string Base = "api";
+
+        public const string Version = "v1";
+
+        public const  string Root = Base + "/" + Version;
+
+        public static class Posts
+        {
+            public const string GetAll = Base + "/" + "posts";
+        }
+    }
+
+
+
+}
---
+++ Controllers/V1/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PostChan.Domain;
+using Microsoft.AspNetCore.Mvc;
+using PostChan.Contracts;
+
+namespace PostChan.Controllers
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+
+            for(int i = 1; i<5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
